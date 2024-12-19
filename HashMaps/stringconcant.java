40c3328e5bc25427ffa4c38c0e6e10b1fcb4ade9
Krubanandan S <krubanandanks@gmail.com>
2024-12-19 23:26:10 +0530

    string

--- HashMaps/stringconcant.java
+++ HashMaps/stringconcant.java
@@ -56,3 +56,44 @@ public class stringconcant {
         }
     }
 }
+
+class Solution {
+    public List<Integer> findSubstring(String s, String[] words) {
+        List<Integer> result = new ArrayList<>();
+
+        HashMap<String, Integer> wordsMap = new HashMap<>();
+
+        for (String word : words) {
+            wordsMap.put(word, wordsMap.getOrDefault(word, 0) + 1);
+        }
+
+        int wordLen = words[0].length();
+        int totalLen = wordLen * words.length;
+
+   for (int i = 0; i <= s.length() - totalLen; i++) {
+    HashMap<String, Integer> seen = new HashMap<>();
+    int nMatch = 0;
+    while (nMatch < words.length) {
+        int start = i + nMatch * wordLen;
+        int end = start + wordLen;
+        String subStr = s.substring(start, end);
+        if (wordsMap.containsKey(subStr)) {
+            seen.put(subStr, seen.getOrDefault(subStr, 0) + 1);
+            if (seen.get(subStr) > wordsMap.get(subStr)) {
+                break;
+            }
+        } else {
+            break;
+        }
+        nMatch++;
+    }
+    if (nMatch == words.length) {
+        result.add(i);
+    }
+}
+
+
+
+        return result;
+    }
+}
